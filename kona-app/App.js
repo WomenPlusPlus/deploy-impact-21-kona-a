9d35646f7c3eed9ec336160b635ba4eb3f8e7f9a
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-10-27 13:53:28 +0200

    Created a start-questionnaire page

--- kona-app/App.js
+++ kona-app/App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Questionnaire from './components/questionnaire/Questionnaire';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{paddingTop: 20} }>
+      <Text> </Text>
+      <Questionnaire /> 
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ kona-app/components/questionnaire/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Image, Text, View} from 'react-native';
+import {globalStyles} from '../globals/GlobalStyles';
+import { questStyles } from './QuestStyles';
+
+const Card = () => {
+    return (
+    <View style={ questStyles.cardStyle }>
+        <Image source={require('../../assets/Logo-warc.png')} style={ globalStyles.card }></Image>
+        <Text style={ questStyles.bold }>Contact</Text>
+        <Text>00403346643445</Text>
+        <Text>Address</Text>
+    </View>
+    )
+}
+
+export default Card
