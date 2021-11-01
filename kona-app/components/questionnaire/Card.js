202192203b4235d4d99cf4f3de78b753c1df4ad3
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-11-01 17:14:08 +0100

    Center question container, adding save(like) organization icon

--- kona-app/components/questionnaire/Card.js
+++ kona-app/components/questionnaire/Card.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../globals/GlobalStyles';
 import { questStyles } from './QuestStyles';
+import { FontAwesome } from '@expo/vector-icons';
+import { useState } from 'react';
+
 
 const OrgCard = () => {
+
+const [save, setSaved] = useState(false);
+
+const pressHandler = () => {
+    setSaved(!save);
+}
+    
     return (
-    <View style={ questStyles.cardStyle }>
-        <Image source={require('../../assets/Logo-warc.png')} style={ globalStyles.card }></Image>
-        <Text style={ questStyles.bold }>Contact</Text>
-        <Text>00403346643445</Text>
-        <Text>Address</Text>
-    </View>
+       <View style={ questStyles.cardStyle }>
+            <Image source={require('../../assets/Logo-warc.png')} style={ globalStyles.card }></Image>
+            <TouchableOpacity onPress={pressHandler}
+                style={{position: 'absolute', right: 8, top: 8 }} >
+                {save ? <FontAwesome name="heart" size={24} color="black" /> : <FontAwesome name="heart-o" size={24} color="black" /> }
+            </TouchableOpacity>
+            <Text style={ questStyles.bold }>Contact</Text>
+            <Text>00403346643445</Text>
+            <Text>Address</Text>
+        </View>
     )
 }
 
