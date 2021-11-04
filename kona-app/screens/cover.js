eadf23aa5f1a47ee5cf4cea43a30bf6462ee4ba8
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-11-04 18:15:57 +0100

    Showing organisations names on the cover page

--- kona-app/screens/cover.js
+++ kona-app/screens/cover.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, FlatList, Text, View } from 'react-native'; 
+import dummy_orgs from '../assets/dummy_orgs.json'
+
+
+const Organisation = require('../assets/dummy_orgs.json')
+
+  const Item = ({ title }) => (
+      <Text> {title} </Text>
+  );
 
 export default function Cover({ navigation }) {
+
+    const renderItem = ({item}) => (
+        <Item title={item.Name} />
+    );
 
     const pressHandler = () => {
         navigation.navigate('StartQuestionnaire')
@@ -9,6 +21,12 @@ export default function Cover({ navigation }) {
 
     return (
         <View style={{paddingTop: 20}}>
+            <FlatList
+                data={Organisation}
+                renderItem={renderItem}
+                keyExtractor={item => item}
+            />
+            <Text>{dummy_orgs.name}</Text>
           <Text>CoverScreen</Text>
           <Button title='Start Questionnaire' onPress={pressHandler} />
         </View>
