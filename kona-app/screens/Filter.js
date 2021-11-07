d7e31079ffdd671f7883ae44a2f5b685afb75890
Alexandra Boedtker <alexboedtker@gmail.com>
2021-11-07 10:38:28 +0100

    initial JSON connection

---
+++ kona-app/screens/Filter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { GlobalStyles } from '../components/globals/GlobalStyles';
+import FlatButton from '../components/globals/Button';
+import BigButton from '../components/globals/BigButton';
+import { QuestionStyles } from '../components/question/QuestionStyles';
+
+export default function Filter({ navigation }) {
+
+    return (
+      <View style={GlobalStyles.container}>
+        <Text style={GlobalStyles.titleTextHomepage}>Let's narrow down your results</Text>
+        <View style={QuestionStyles.cardsContainerLeft}>
+          <BigButton answer='Food' />
+          <BigButton answer='Shelter' />
+          <BigButton answer='Education' />
+          <BigButton answer='Agriculture' />
+          <BigButton answer='Social Protection' />
+        </View>
+      </View>
+
+    )
+}
--- kona-app/screens/home.js
+++ kona-app/screens/home.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import { GlobalStyles } from '../components/globals/GlobalStyles';
 import FlatButton from '../components/globals/Button';
+import BigButton from '../components/globals/BigButton';
+import Data from '../assets/dummy_orgs.json';
+import { QuestionStyles } from '../components/question/QuestionStyles';
 
 export default function Home({ navigation }) {
 
@@ -9,10 +12,23 @@ export default function Home({ navigation }) {
         navigation.navigate('Cover')
     }
 
+    const pressFilter = () => {
+        navigation.navigate('Filter', Data)
+    }
+
     return (
       <View style={GlobalStyles.container}>
-        <Text style={GlobalStyles.titleTextHomepage}>Welcome to DOTS!</Text>
+        { Data.forEach(org => console.log(org['Name'])) }
+        <Text style={GlobalStyles.titleTextHomepage}>What do you need help with?</Text>
+        <View style={QuestionStyles.cardsContainerLeft}>
+          <BigButton answer='Food' onPress={pressFilter} />
+          <BigButton answer='Shelter' onPress={pressFilter} />
+          <BigButton answer='Education' onPress={pressFilter} />
+          <BigButton answer='Agriculture' onPress={pressFilter} />
+          <BigButton answer='Social Protection' onPress={pressFilter} />
+        </View>
         <FlatButton text='Cover Page' onPress={pressHandler} />
       </View>
+
     )
 }
