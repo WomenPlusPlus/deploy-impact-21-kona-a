70a4a7506659819fff5b9b72cbf5db29bcd183f3
KatKava <katryn.kav97@gmail.com>
2021-11-11 14:39:31 +0000

    Initial styling for homepage complete. Search bad styled, input width adjusted to 100% as well as placeholder styling. Padding and overall style of container also fixed to resemble original UX design

--- kona-app/components/globals/GlobalStyles.js
+++ kona-app/components/globals/GlobalStyles.js
@@ -1,149 +1,162 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const GlobalStyles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        padding: 24,
-        backgroundColor: 'rgb(242,240,240)'
-    },
-    whiteContainer: {
-        flexDirection: 'column',
-        margin: 20,
-    },
-    greyContainer: {
-        backgroundColor: '#EAEAEA',
-    },
-    topFilterSection: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    flexDirectionColumn: {
-      flexDirection: 'column',
-    },
-    titleTextHomepage: {
-      fontFamily: 'poppins-medium',
-      fontSize: 24,
-      lineHeight: 36,
-      color: '#6B6B6B',
-    },
-    titleTextBold: {
-      fontFamily: 'poppins-bold',
-      fontSize: 16,
-      lineHeight: 24,
-      color: '#212121',
-    },
-    normalText: {
-      fontFamily: 'poppins-medium',
-      fontSize: 16,
-      lineHeight: 24,
-      color: '#212121',
-    },
-    placeholderText: {
-      fontFamily: 'poppins-regular',
-      fontSize: 16,
-      lineHeight: 24,
-      color: '#6B6B6B',
-    },
-    card: {
-        width: 100,
-        height: 100,
-    },
-    button: {
-      alignItems: 'center',
-      paddingVertical: 15,
-      paddingHorizontal: 20,
-      backgroundColor: '#212121',
-      borderRadius: 10,
-    },
-    buttonText: {
-      fontFamily: 'poppins-medium',
-      fontSize: 16,
-      lineHeight: 24,
-      color: '#ffffff',
-      marginVertical: 0,
-      marginHorizontal: 10,
-    },
-    textLink: {
-        paddingTop: 10,
-        marginRight: 20,
-        textDecorationLine: 'underline',
-        textAlign: 'right',
-        fontWeight: 'bold'
-    },
-    bigButton: {
-      backgroundColor: '#F5F5F5',
-      height: 54,
-      borderRadius: 10,
-      marginVertical: 5,
-      paddingVertical: 15,
-      paddingHorizontal: 20,
-      alignSelf: 'flex-start',
-    },
-    cardsContainer: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      marginVertical: 16
-    },
-    cardsContainerLeft: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-between',
-      marginVertical: 16
-    },
-    pt: {
-      paddingTop: 10
-    },
-    questionContainer: {
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'flex-start',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      height: '100%'
-    },
-    smallerQuestionContainer: {
-      alignSelf: 'center'
-    },
-    questionText: {
-      fontFamily: 'poppins-medium',
-      fontSize: 16,
-      lineHeight: 24,
-      color: '#212121',
-    },
-    searchContainer: {
-      backgroundColor: 'white',
-      padding: 10,
-      borderRadius: 10,
-      flex: 1,
-      flexDirection: 'row',
-      borderWidth: 1,
-      borderColor: "#6B6B6B",
-      height: 52,
-    },
-    searchIcon: {
-      paddingRight: 10,
-      color: '#6B6B6B',
-      fontSize: 16,
-      paddingTop: 2,
-    },
-    itemStyle: {
-      padding: 10,
-    },
-    ptpb: {
-      paddingTop: 10,
-      paddingBottom: 10,
-    }, 
-    supportContainer: {
-      flex: 1,
-      },
-    bannerText: {
-      fontFamily: 'poppins-medium',
-      fontSize: 24,
-      lineHeight: 36,
-      color: '#6B6B6B',
-      paddingHorizontal: 10,
-      paddingVertical: 100
-    }
-      
-})
+  container: {
+    flexDirection: "column",
+    padding: 24,
+    backgroundColor: "rgb(242,240,240)",
+  },
+  homepageContainer: {
+    position: "relative",
+    textAlign: "left",
+  },
+  handImage: {
+    width: 189,
+    height: 180,
+    position: "absolute",
+    right: -25,
+    top: 88,
+  },
+  whiteContainer: {
+    flexDirection: "column",
+    margin: 20,
+  },
+  greyContainer: {
+    backgroundColor: "#EAEAEA",
+  },
+  topFilterSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  flexDirectionColumn: {
+    flexDirection: "column",
+  },
+  titleTextHomepage: {
+    fontFamily: "poppins-medium",
+    fontSize: 24,
+    lineHeight: 36,
+    color: "#6B6B6B",
+  },
+  titleTextBold: {
+    fontFamily: "poppins-bold",
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#212121",
+  },
+  normalText: {
+    fontFamily: "poppins-medium",
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#212121",
+  },
+  placeholderText: {
+    fontFamily: "poppins-regular",
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#6B6B6B",
+  },
+  card: {
+    width: 100,
+    height: 100,
+  },
+  button: {
+    alignItems: "center",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    backgroundColor: "#212121",
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontFamily: "poppins-medium",
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#ffffff",
+    marginVertical: 0,
+    marginHorizontal: 10,
+  },
+  textLink: {
+    paddingTop: 10,
+    marginRight: 20,
+    textDecorationLine: "underline",
+    textAlign: "right",
+    fontWeight: "bold",
+  },
+  bigButton: {
+    backgroundColor: "#F5F5F5",
+    height: 54,
+    borderRadius: 10,
+    marginVertical: 5,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    alignSelf: "flex-start",
+  },
+  cardsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    marginVertical: 16,
+  },
+  cardsContainerLeft: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginVertical: 16,
+  },
+  pt: {
+    paddingTop: 10,
+  },
+  questionContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    height: "100%",
+  },
+  smallerQuestionContainer: {
+    alignSelf: "center",
+  },
+  questionText: {
+    fontFamily: "poppins-medium",
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#212121",
+  },
+  searchContainer: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "#6B6B6B",
+    height: 52,
+  },
+  input: {
+    width: "100%",
+  },
+  searchIcon: {
+    paddingRight: 10,
+    color: "#6B6B6B",
+    fontSize: 16,
+    paddingTop: 2,
+  },
+  itemStyle: {
+    padding: 10,
+  },
+  ptpb: {
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  supportContainer: {
+    flex: 1,
+  },
+  bannerText: {
+    fontFamily: "poppins-medium",
+    fontSize: 24,
+    lineHeight: 36,
+    color: "#6B6B6B",
+    paddingRight: 20,
+    paddingVertical: 100,
+  },
+});
--- kona-app/components/globals/SearchBar.js
+++ kona-app/components/globals/SearchBar.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { FlatList, Text, View } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import { GlobalStyles } from './GlobalStyles';
-import { FontAwesome } from '@expo/vector-icons';
+import React from "react";
+import { useState, useEffect } from "react";
+import { FlatList, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { GlobalStyles } from "./GlobalStyles";
+import { FontAwesome } from "@expo/vector-icons";
 
-export default function SearchBar ({ placeholder, data, setSearchQuery }) {
-    const handleFilter = (text) => {
-            setSearchQuery(text)
-    }
-    return (
-        <View >
-            <View style={GlobalStyles.searchContainer}>
-                <FontAwesome style={GlobalStyles.searchIcon} name="search" />
-                <TextInput
-                 style={GlobalStyles.placeholderText}
-                placeholder="Search"
-                onChangeText={handleFilter}/>
-            </View>    
-        </View>
-    );
-  }
-
+export default function SearchBar({ placeholder, data, setSearchQuery }) {
+  const handleFilter = (text) => {
+    setSearchQuery(text);
+  };
+  return (
+    <View style={{ paddingBottom: 51 }}>
+      <View style={GlobalStyles.searchContainer}>
+        <FontAwesome style={GlobalStyles.searchIcon} name="search" />
+        <TextInput
+          style={[GlobalStyles.placeholderText, GlobalStyles.input]}
+          placeholder="Search"
+          onChangeText={handleFilter}
+        />
+      </View>
+    </View>
+  );
+}
