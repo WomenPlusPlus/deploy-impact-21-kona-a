2e97790cf11a56d1e1d9faeb209845c174cd6dc5
KatKava <katryn.kav97@gmail.com>
2021-11-17 16:36:58 +0000

    Dynamic rendering for SDGs in homepage now

--- kona-app/assets/iconImages.js
+++ kona-app/assets/iconImages.js
@@ -1,22 +1,77 @@
-const iconImages = {
-	noPovertyIcon: require("./SDG-Icons/E-WEB-Goal-01.png"),
-    noHungerIcon: require("./SDG-Icons/E-WEB-Goal-02.png"),
-    healthIcon: require("./SDG-Icons/E-WEB-Goal-03.png"),
-    educationIcon: require("./SDG-Icons/E-WEB-Goal-04.png"),
-    genderEqualityIcon: require("./SDG-Icons/E-WEB-Goal-05.png"),
-    waterIcon: require("./SDG-Icons/E-WEB-Goal-06.png"),
-    cleanEnergyIcon: require("./SDG-Icons/E-WEB-Goal-07.png"),
-    workIcon: require("./SDG-Icons/E-WEB-Goal-08.png"),
-    industryIcon: require("./SDG-Icons/E-WEB-Goal-09.png"),
-    inequalitiesIcon: require("./SDG-Icons/E-WEB-Goal-10.png"),
-    sustainableComIcon: require("./SDG-Icons/E-WEB-Goal-11.png"),
-    consumptionIcon: require("./SDG-Icons/E-WEB-Goal-12.png"),
-    climateIcon: require("./SDG-Icons/E-WEB-Goal-13.png"),
-    belowWaterIcon: require("./SDG-Icons/E-WEB-Goal-14.png"),
-    onLandIcon: require("./SDG-Icons/E-WEB-Goal-15.png"),
-    justiceIcon: require("./SDG-Icons/E-WEB-Goal-16.png"),
-    partnershipIcon: require("./SDG-Icons/E-WEB-Goal-17.png")
-};
+// const iconImages = {
+// 	noPovertyIcon: require("./SDG-Icons/E-WEB-Goal-01.png"),
+//     noHungerIcon: require("./SDG-Icons/E-WEB-Goal-02.png"),
+//     healthIcon: require("./SDG-Icons/E-WEB-Goal-03.png"),
+//     educationIcon: require("./SDG-Icons/E-WEB-Goal-04.png"),
+//     genderEqualityIcon: require("./SDG-Icons/E-WEB-Goal-05.png"),
+//     waterIcon: require("./SDG-Icons/E-WEB-Goal-06.png"),
+//     cleanEnergyIcon: require("./SDG-Icons/E-WEB-Goal-07.png"),
+//     workIcon: require("./SDG-Icons/E-WEB-Goal-08.png"),
+//     industryIcon: require("./SDG-Icons/E-WEB-Goal-09.png"),
+//     inequalitiesIcon: require("./SDG-Icons/E-WEB-Goal-10.png"),
+//     sustainableComIcon: require("./SDG-Icons/E-WEB-Goal-11.png"),
+//     consumptionIcon: require("./SDG-Icons/E-WEB-Goal-12.png"),
+//     climateIcon: require("./SDG-Icons/E-WEB-Goal-13.png"),
+//     belowWaterIcon: require("./SDG-Icons/E-WEB-Goal-14.png"),
+//     onLandIcon: require("./SDG-Icons/E-WEB-Goal-15.png"),
+//     justiceIcon: require("./SDG-Icons/E-WEB-Goal-16.png"),
+//     partnershipIcon: require("./SDG-Icons/E-WEB-Goal-17.png")
+// };
 
+const iconImages = [
+    {
+        name: "No Poverty",
+        image: require("./SDG-Icons/E-WEB-Goal-01.png")
+    },
+   {
+        name: "No Hunger",
+        image: require("./SDG-Icons/E-WEB-Goal-02.png")
+    },
 
+    {   name: "Health",
+        image: require("./SDG-Icons/E-WEB-Goal-03.png")
+    },
+    {   name: "Education",
+        image: require("./SDG-Icons/E-WEB-Goal-04.png")
+    },
+    {   name: "Gender Equality",
+        image: require("./SDG-Icons/E-WEB-Goal-05.png")
+    },
+    {   name: "Water/Sanitation",
+        image: require("./SDG-Icons/E-WEB-Goal-06.png")
+    },
+    {   name: "Clean Energy",
+        image: require("./SDG-Icons/E-WEB-Goal-07.png")
+    },
+    {   name: "Work / Economic Growth",
+        image: require("./SDG-Icons/E-WEB-Goal-08.png")
+    },
+    {   name: "Industry / Innovation / Infrastructure",
+        image: require("./SDG-Icons/E-WEB-Goal-09.png")
+    },
+    {   name: "Inequalities",
+        image: require("./SDG-Icons/E-WEB-Goal-10.png")
+    },
+    {   name: "Sustainable Communities",
+        image: require("./SDG-Icons/E-WEB-Goal-11.png")
+    },
+    {   name: "Responsible Consumption",
+        image: require("./SDG-Icons/E-WEB-Goal-12.png")
+    },
+    {   name: "Climate",
+        image: require("./SDG-Icons/E-WEB-Goal-13.png")
+    },
+    {   name: "Life below Water",
+        image: require("./SDG-Icons/E-WEB-Goal-14.png")
+    },
+    {   name: "Life on land",
+        image: require("./SDG-Icons/E-WEB-Goal-15.png")
+    },
+    {   name: "Justice / Institutions",
+        image: require("./SDG-Icons/E-WEB-Goal-16.png")
+    },
+    {   name: "Partnership for the Goals",
+        image: require("./SDG-Icons/E-WEB-Goal-17.png")
+    }
+]
 export default iconImages;
