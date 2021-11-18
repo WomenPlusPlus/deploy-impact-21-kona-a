0551a63ec78bc93920da271a17ab09965dc6466b
Alexandra Boedtker <alexboedtker@gmail.com>
2021-11-18 09:24:09 +0100

    added arrays with search keywords

--- kona-app/screens/Accordion.js
+++ kona-app/screens/Accordion.js
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
 import { Text, View, ScrollView } from 'react-native';
 import { GlobalStyles } from '../components/globals/GlobalStyles';
-import BigButton from '../components/globals/BigButton';
-import CheckButton from '../components/globals/CheckButton';
+import ResultsButton from '../components/question/ResultsButton';
 import { QuestionStyles } from '../components/question/QuestionStyles';
 import Data from '../assets/kona_orgs.json';
 import AccordionFilter from '../components/question/Accordion';
 
 export default function Accordion({ route, navigation }) {
-  
+
   const receivedKeywords = route.params;
   console.log("We received this from the Filtered Orgs:")
   console.log(receivedKeywords);
-  
+
   const initialKeywordSet = new Set();
   initialKeywordSet.add('food');
 
   const [selectedKeywords, setSelectedKeywords] = useState(initialKeywordSet);
 
-  const mainCategories = ['Food', 'Shelter', 'Education', 'Agriculture', 'Social Protection']
+  // array with main categories
+  const mainCategories = ['Health', 'Education', 'Social Services', 'Agriculture', 'Legal', 'Migration', 'Employment', 'Crisis']
+  // arrays of sub categories
+  const healthSub = ['Disable', 'Sex', 'Maternal', 'Disease', '%AIDS%', 'Healthcare', 'Nutrition', 'Hygiene']
+  const educationSub = ['Training',	'Financ%',	'Facilities',	'%AIDS%',	'Entrepreneur']
+  const socialServicesSub = ['%Violence%',	'%Victim%',	'Safety',	'Poverty',	'Sanitation',	'Water	Prison',	'Protection',	'LBGT']
+  const agricultureSub = ['Technology',	'Education',	'Seed', 'Donation',	'Financial',	'Agriculture', 'Development']
+  const legalSub = ["Women's Rights",	"Children's Rights",	"Human Trafficking",	"Security",	"Child Protection",	"Reintegration",	"Political Justice",	"Discrimination",	"Advisory",	"Victim Advocacy",	"Victim"]
+  const migrationSub = ['Refugee',	'Repatriation',	'Asylum',	'%Integration%',	'Citizenship']
+  const employmentSub = ['Training%',	'Entrepreneur%',	'Technical%',	'Partnerships',	'Financ%',	'Apprenticeship']
+  const crisisSub = ['Humanitarian', 	'Emergencies',	'Disaster',	'Human Trafficking',	'War']
+  //
+
   const targetGroups = ['Victims of domestic Violence', 'Refugees', 'Homeless People', 'Disabled People', 'LGBTQIA+', 'Migrants']
   const filterBy = (selectedCat, buttonNames) => {
     const filteredCats = buttonNames.filter((cat) => {
@@ -68,6 +79,7 @@ export default function Accordion({ route, navigation }) {
             <View>
               <AccordionFilter onCheck={onCheckHandler} onUncheck={onUncheckHandler}/>
             </View>
+            <ResultsButton filteredOrgs={}/>
       </ScrollView>
     )
 }
