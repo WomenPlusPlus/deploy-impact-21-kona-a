ce49afce65975a757139162b58bd59dd7cd09ec6
KatKava <katryn.kav97@gmail.com>
2021-11-26 14:46:55 +0000

    Search page styling partial

--- kona-app/screens/SearchPage.js
+++ kona-app/screens/SearchPage.js
@@ -1,7 +1,10 @@
 import React from "react"; 
 import { StackRouter } from "react-navigation";
 import SearchBar from "../components/globals/SearchBar";
+import { Text, View } from "react-native";
+import { GlobalStyles } from "../components/globals/GlobalStyles";
 import { filterByKeywordInAnyField } from "../components/globals/FilterUtils";
+import MainButton from "../components/globals/MainButton";
 import Data from "../assets/kona_orgs.json";
 
 export default function SearchPage({route, navigation}) {
@@ -20,9 +23,39 @@ export default function SearchPage({route, navigation}) {
          filterByFieldsAndNavigate(["MainCategory", "SubCategory"], "FilteredOrgs", searchQuery) 
     }
 
+    const filterByMainOrSubCat = (category) => (filterByFieldsAndNavigate(["MainCategory", "SubCategory"], "FilteredOrgs", category));
+
     return (
-        <div>
-            <SearchBar initialSearchQuery={route.params} onSubmit={navigateToFilteredOrgs} autoFocus /*key="searchPage"*//>     
-        </div>
+      <View style={GlobalStyles.whiteContainer}>
+        <View>
+        <Text style={[GlobalStyles.normalText, GlobalStyles.ptpb]}>What do you need help with?</Text>
+        </View>
+            <SearchBar style={{backgroundColor: "white"}} initialSearchQuery={route.params} onSubmit={navigateToFilteredOrgs} autoFocus /*key="searchPage"*//>  
+            
+        <View style={{marginTop: -15}}>
+        <MainButton 
+          answer="Food"
+          iconName="food-variant"
+          onPressWithParam={filterByMainOrSubCat} />
+        <MainButton 
+          answer="Shelter"
+          iconName="home-heart" 
+          onPressWithParam={filterByMainOrSubCat} />
+        <MainButton 
+          answer="Education" 
+          iconName="book-open-page-variant"
+          onPressWithParam={filterByMainOrSubCat} />
+        <MainButton
+          answer="Health"
+          iconName="medical-bag"
+          onPressWithParam={filterByMainOrSubCat}
+        />
+        <MainButton
+          answer="Employment"
+          iconName="account-hard-hat"
+          onPressWithParam={filterByMainOrSubCat}
+        />
+        </View>
+      </View>   
     )
 }
